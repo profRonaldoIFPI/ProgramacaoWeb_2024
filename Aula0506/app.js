f2f00f6577c6fd493a25af29b8489ab31e229790
ronaldopborges <ronaldo.borges@alunos.ufersa.edu.br>
2024-06-12 08:44:04 -0300

    Aula de 12/06

--- Aula0506/app.js
+++ Aula0506/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs'); // manipular artquivos
+const { stringify } = require('querystring');
 const PORT = 3000;
 const app = express();
 app.set('view engine', 'ejs')
@@ -7,8 +8,27 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}));
 //linha de baixo configura o diretório de conteúdos staticos
 app.use(express.static('public'));
+function loadFile(){
+    try{
+        const data = fs.readFileSync("tasks.json");    
+        return JSON.parse(data); //converte para uma lista JSON    
+    } catch (err) {
+        console.log(`Erro ao carregar o arquivo: ${err}`);
+        return [];
+    }
+}
+function seveFile(data){
+    fs.writeFileSync(        // grava um arquivo de forma sincrona
+        'tasks.json',        
+        JSON.stringify(data) // converte JSON para string
+    );
+}
 app.get('/tasks', (req,res)=>{
-    res.render('index');
+    let tarefas = loadFile();
+    console.log(tarefas);
+    res.render('index', {
+        tasks: tarefas
+    });
 });
 
 app.get('/tasks/new', (req,res)=>{
@@ -16,20 +36,28 @@ app.get('/tasks/new', (req,res)=>{
 });
 
 app.post('/tasks',(req,res)=>{
-    // criar/carregar o arquivo
-    // adicionar conteudo da req a uma vetor(array)
-    // salvar o arquivo 
+    tasks = loadFile();
+    newTask = {
+        id: Date.now().toString(),
+        title: req.body.title,
+        completed: false 
+    }
+    tasks.push(newTask);
+    seveFile(tasks); 
 });
+
 app.post('/tasks/:id/complete',(req,res)=>{
     // carregar arquivo
     // buscar por id
     // salvas o arquivo
 });
+
 app.post('/tasks/:id/delete',(req,res)=>{
     // carregar arquivo
     // buscar por id
     // salvas o arquivo
 });
+
 app.listen(PORT,(err)=>{
     console.log('Servidor online')
 });
